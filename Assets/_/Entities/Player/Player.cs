6ec1b61af39c26f62e6053f956cca1199e20548b
dfcut8 <dfcut8@proton.me>
2025-04-03 10:21:59 -0400

    add tourgue

--- Assets/_/Entities/Player/Player.cs
+++ Assets/_/Entities/Player/Player.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] float tourque = 1f;
+    private Rigidbody2D rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Get the input from the player
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        rb.AddTorque(moveHorizontal * tourque, ForceMode2D.Force);
     }
 }
