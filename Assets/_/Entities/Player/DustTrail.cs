0ea4ad311391add8c2f32aa968d093d52e9d6fa2
dfcut8 <dfcut8@proton.me>
2025-04-07 08:31:34 -0400

    add dust effect

---
+++ Assets/_/Entities/Player/DustTrail.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class DustTrail : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem dustEffect;
+    void Start()
+    {
+        if (dustEffect == null)
+        {
+            dustEffect = GetComponent<ParticleSystem>();
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            if (dustEffect != null)
+            {
+                dustEffect.Play();
+            }
+        }
+    }
+
+    void OllisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            if (dustEffect != null)
+            {
+                dustEffect.Stop();
+            }
+
+        }
+    }
+}
