6c4551ce395aa9e7bc6e201c507005e7121b3705
dfcut8 <dfcut8@proton.me>
2025-04-07 09:11:04 -0400

    add sound effects

--- Assets/_/Entities/FinishLine/FinishLine.cs
+++ Assets/_/Entities/FinishLine/FinishLine.cs
@@ -6,11 +6,18 @@ public class FinishLine : MonoBehaviour
     [SerializeField] private float delayAfterFinishLine = 2f;
     [SerializeField] private ParticleSystem finishEffect;
 
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             Debug.Log("Player has crossed the finish line!");
+            audioSource.Play();
             finishEffect.Play();
             Invoke("reloadScene", delayAfterFinishLine);
         }
--- Assets/_/Entities/Player/CrashDetector.cs
+++ Assets/_/Entities/Player/CrashDetector.cs
@@ -5,10 +5,14 @@ public class CrashDetector : MonoBehaviour
 {
     [SerializeField] private float delayAfterCrash = 1f;
     [SerializeField] private ParticleSystem hitEffect;
-    CircleCollider2D cc2d;
+    [SerializeField] private AudioClip crashSFX;
+
+    private CircleCollider2D cc2d;
+    private AudioSource audioSource;
     void Start()
     {
       cc2d = GetComponent<CircleCollider2D>();
+      audioSource = GetComponent<AudioSource>();
     }
     void OnTriggerEnter2D(Collider2D collision)
     {
@@ -16,6 +20,7 @@ public class CrashDetector : MonoBehaviour
         {
             Debug.Log("Player hit the ground!");
             hitEffect.Play();
+            audioSource.PlayOneShot(crashSFX);
             Invoke("reloadScene", delayAfterCrash);
         }
     }
