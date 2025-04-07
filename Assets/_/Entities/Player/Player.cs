5a67a7fb84fca3043e4336f3f32e9e03868c1c23
dfcut8 <dfcut8@proton.me>
2025-04-07 08:18:16 -0400

    add boost

--- Assets/_/Entities/Player/Player.cs
+++ Assets/_/Entities/Player/Player.cs
@@ -3,18 +3,34 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float tourque = 10f;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float baseSpeed = 10f;
     private Rigidbody2D rb;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private SurfaceEffector2D surfaceEffector2D;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindAnyObjectByType<SurfaceEffector2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         // Get the input from the player
         float moveHorizontal = Input.GetAxis("Horizontal");
         rb.AddTorque(-moveHorizontal * tourque);
+        responseToBoost();
+    }
+
+    private void responseToBoost()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+        else
+        {
+            surfaceEffector2D.speed = baseSpeed;
+        }
     }
 }
